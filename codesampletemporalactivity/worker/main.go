bfe684d511ceb866e7a432fbc356ae45dbf54052
genos1998 <shaikhjunaid1998@gmail.com>
2022-10-20 00:19:28 +0530

    Activity example
    Signed-off-by: genos1998 <shaikhjunaid1998@gmail.com>

---
+++ codesampletemporalactivity/worker/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"greeting"
+	"log"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+func main() {
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, "greeting-tasks", worker.Options{})
+
+	w.RegisterWorkflow(greeting.GreetSomeone)
+	w.RegisterActivity(greeting.GreetInSpanish)
+	w.RegisterActivity(greeting.FarewellInSpanish)
+
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
